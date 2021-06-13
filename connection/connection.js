318436bc11ebd3e18d737665c4e022cfae62ef39
Abhijith Uthaman <abhijithuthaman@outlook.com>
2021-06-13 11:46:55 +0530

    BugFix

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "tracker",
-  database: "student_tracker",
+  database: "tracker",
 });
 
 connection.connect((error) => {
