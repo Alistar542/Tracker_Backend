4db7f59c939d19d027c68780b7014500841c3953
Abhijith018 <abhijithuthaman@outlook.com>
2021-06-13 16:37:04 +0530

    Bugfix

--- src/constants/userquery.js
+++ src/constants/userquery.js
@@ -3,4 +3,4 @@ exports.LOGIN_USER_QUERY = `SELECT * FROM user WHERE userName=? and officeCode=?
 exports.CREATE_USER_QUERY = `INSERT INTO user (userName,password,userType,officeCode) values
 (?,?,?,?)`;
 
-exports.LIST_USER_QUERY=`SELECT * FROM USER `
+exports.LIST_USER_QUERY=`SELECT * FROM user `
